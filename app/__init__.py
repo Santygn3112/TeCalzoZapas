76a2a41742560387afa1e7b52122b61c26e4cc08
Santygn3112 <guillennazareno@gmail.com>
2025-05-12 19:05:29 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,9 +11,7 @@ mysql = MySQL(app)
 # Configurar Flask-Login
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-# Cambia aquí el mensaje por defecto
 login_manager.login_message = 'Debes iniciar sesión para continuar.'
-# (opcional) Categoría del flash: 'info', 'warning', 'danger', etc.
 login_manager.login_message_category = 'warning'
 
 from app import routes, models
